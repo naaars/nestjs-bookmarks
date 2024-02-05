3a0c6fe51ce87568c9362477d1346e7a06635dce
Inigo Moreno <inigomorenotato@gmail.com>
2024-02-05 08:35:42 +0100

    Update package name and add validation to signup endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { User, Bookmark } from "@prisma/client"
+import { PrismaService } from "../prisma/prisma.service";
+import { AuthDto } from "./dto";
 
-@Injectable({}) 
+@Injectable() 
 export class AuthService {
-    signup() {
+    constructor(private prisma: PrismaService) {}
+    signup(dto: AuthDto) {
         return  'I have signed up';
     }
 
